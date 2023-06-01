294acec3c565e2fd2f197df4f7d33fef79c21d26
ywt <2021201613@ruc.edu.cn>
2023-06-01 15:08:27 +0800

    update network frame

--- code/infoplace/core/src/com/rucjava/infoplace/ControllerModule/NetworkModule/UploadSystem.java
+++ code/infoplace/core/src/com/rucjava/infoplace/ControllerModule/NetworkModule/UploadSystem.java
@@ -4,4 +4,9 @@ public class UploadSystem {
     public void uploadPixels(PixelDataPack dataPack) {
 
     }
+
+    // 每幅画应该有唯一的名字，由服务端分配，tarPictureName 是画的名字，comment 是用户对画的评论
+    public void uploadComment(String tarPictureName, String comment) {
+
+    }
 }
